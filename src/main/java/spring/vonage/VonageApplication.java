1b5f8236006ab8b1f7f9ed09d5b7f2aabe8f995b
goyois <bau12288@gmail.com>
2023-07-11 14:59:59 +0900

    first commit

---
+++ src/main/java/spring/vonage/VonageApplication.java
@@ -0,0 +1,13 @@
+package spring.vonage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VonageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VonageApplication.class, args);
+	}
+
+}
